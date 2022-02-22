e1100e1280ce119a79be101d1cde1819a9d0e931
CallPhysical <100102750+CallPhysical@users.noreply.github.com>
2022-02-22 23:52:44 +0900

    Add files via upload

--- drones_4_wave.ino
+++ drones_4_wave.ino
@@ -1,6 +1,9 @@
 /* 
-    8knobs Drones
+    Based on 8knobs Drones
     Analog Inputs: A0-A5, A6 - A11 (on digital pins 4, 6, 8, 9, 10, and 12). 
+    Modified to switch waveforms using toggle switch on pin 2, 3
+    Only check one of the eight pots each cycle to avoid clicking at high CONTROL_RATE
+    
 */
 
 #include <MozziGuts.h>
@@ -9,7 +12,7 @@
 #include <tables/cos2048_int8.h>
 #include <tables/triangle_valve_2_2048_int8.h>
 #include <tables/saw2048_int8.h>
-#define CONTROL_RATE 16
+#define CONTROL_RATE 64
 
 // harmonics
 Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aCos1(SIN2048_DATA);
@@ -33,7 +36,7 @@ Oscil<SIN2048_NUM_CELLS, CONTROL_RATE> kVol8(SIN2048_DATA);
 
 // 
 char v1,v2,v3,v4,v5,v6,v7,v8;
-
+int pot;
 void setup(){
 
   // set harmonic frequencies
@@ -57,6 +60,7 @@ void setup(){
   kVol8.setFreq(0.041f);
 
   v1=v2=v3=v4=v5=v6=v7=v8=127;
+  pot=0;
   pinMode(2, INPUT_PULLUP); 
   pinMode(3, INPUT_PULLUP);   
   startMozzi(CONTROL_RATE);
@@ -68,100 +72,129 @@ void loop(){
 
 
 void updateControl(){
-  int sensorVal1 = digitalRead(2); 
-  int sensorVal2 = digitalRead(3); 
-  if (sensorVal1 == HIGH && sensorVal2 == HIGH) 
-  {
-    aCos1.setTable(SIN2048_DATA);
-    aCos2.setTable(SIN2048_DATA);
-    aCos3.setTable(SIN2048_DATA);
-    aCos4.setTable(SIN2048_DATA);
-    aCos5.setTable(SIN2048_DATA);
-    aCos6.setTable(SIN2048_DATA);
-    aCos7.setTable(SIN2048_DATA);
-    aCos8.setTable(SIN2048_DATA);  
+  int sensorVal1; 
+  int sensorVal2;
 
-    kVol1.setTable(SIN2048_DATA);
-    kVol2.setTable(SIN2048_DATA);
-    kVol3.setTable(SIN2048_DATA);
-    kVol4.setTable(SIN2048_DATA);
-    kVol5.setTable(SIN2048_DATA);
-    kVol6.setTable(SIN2048_DATA);
-    kVol7.setTable(SIN2048_DATA);
-    kVol8.setTable(SIN2048_DATA);  
+  
+  pot++; // select the next pot number
+  if (pot==9) { // reset the pot number back to zero
+    pot=0;
+    }
+  switch (pot) {
+    case 0:
+    aCos1.setFreq((mozziAnalogRead(A0))*4);
+    break;
+    case 1:
+    aCos2.setFreq((mozziAnalogRead(A1))*4);
+    break;
+    case 2:
+    aCos3.setFreq((mozziAnalogRead(A2))*4);
+    break;   
+    case 3:
+    aCos4.setFreq((mozziAnalogRead(A3))*4);
+    break;
+    case 4:
+    aCos5.setFreq((mozziAnalogRead(A4))*4);
+    break;
+    case 5:
+    aCos6.setFreq((mozziAnalogRead(A5))*4);
+    break;
+    case 6:
+    aCos7.setFreq((mozziAnalogRead(A6))*4);
+    break;
+    case 7:
+    aCos8.setFreq((mozziAnalogRead(A7))*4);
+    break;
+    case 8:
+    // every ninth cycle also check the waveform switches
+    sensorVal1= digitalRead(2); 
+    sensorVal2 = digitalRead(3); 
+    if (sensorVal1 == HIGH && sensorVal2 == HIGH) 
+    {
+      aCos1.setTable(SIN2048_DATA);
+      aCos2.setTable(SIN2048_DATA);
+      aCos3.setTable(SIN2048_DATA);
+      aCos4.setTable(SIN2048_DATA);
+      aCos5.setTable(SIN2048_DATA);
+      aCos6.setTable(SIN2048_DATA);
+      aCos7.setTable(SIN2048_DATA);
+      aCos8.setTable(SIN2048_DATA);  
+  
+      kVol1.setTable(SIN2048_DATA);
+      kVol2.setTable(SIN2048_DATA);
+      kVol3.setTable(SIN2048_DATA);
+      kVol4.setTable(SIN2048_DATA);
+      kVol5.setTable(SIN2048_DATA);
+      kVol6.setTable(SIN2048_DATA);
+      kVol7.setTable(SIN2048_DATA);
+      kVol8.setTable(SIN2048_DATA);  
+      
+    }
+    else if (sensorVal1 == HIGH && sensorVal2 == LOW) 
+    {
+      aCos1.setTable(COS2048_DATA);
+      aCos2.setTable(COS2048_DATA);
+      aCos3.setTable(COS2048_DATA);
+      aCos4.setTable(COS2048_DATA);
+      aCos5.setTable(COS2048_DATA);
+      aCos6.setTable(COS2048_DATA);
+      aCos7.setTable(COS2048_DATA);
+      aCos8.setTable(COS2048_DATA);  
+  
+      kVol1.setTable(COS2048_DATA);
+      kVol2.setTable(COS2048_DATA);
+      kVol3.setTable(COS2048_DATA);
+      kVol4.setTable(COS2048_DATA);
+      kVol5.setTable(COS2048_DATA);
+      kVol6.setTable(COS2048_DATA);
+      kVol7.setTable(COS2048_DATA);
+      kVol8.setTable(COS2048_DATA);  
+    }
+    else if (sensorVal1 == LOW && sensorVal2 == HIGH) 
+    {
+      aCos1.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos2.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos3.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos4.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos5.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos6.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos7.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      aCos8.setTable(TRIANGLE_VALVE_2_2048_DATA);  
+  
+      kVol1.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol2.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol3.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol4.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol5.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol6.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol7.setTable(TRIANGLE_VALVE_2_2048_DATA);
+      kVol8.setTable(TRIANGLE_VALVE_2_2048_DATA);  
+    }
+     else
+    {
+      aCos1.setTable(SAW2048_DATA);
+      aCos2.setTable(SAW2048_DATA);
+      aCos3.setTable(SAW2048_DATA);
+      aCos4.setTable(SAW2048_DATA);
+      aCos5.setTable(SAW2048_DATA);
+      aCos6.setTable(SAW2048_DATA);
+      aCos7.setTable(SAW2048_DATA);
+      aCos8.setTable(SAW2048_DATA);  
+  
+      kVol1.setTable(SAW2048_DATA);
+      kVol2.setTable(SAW2048_DATA);
+      kVol3.setTable(SAW2048_DATA);
+      kVol4.setTable(SAW2048_DATA);
+      kVol5.setTable(SAW2048_DATA);
+      kVol6.setTable(SAW2048_DATA);
+      kVol7.setTable(SAW2048_DATA);
+      kVol8.setTable(SAW2048_DATA);  
+    } 
+    break;
+
+    }  // end of switch
     
-  }
-  else if (sensorVal1 == HIGH && sensorVal2 == LOW) 
-  {
-    aCos1.setTable(COS2048_DATA);
-    aCos2.setTable(COS2048_DATA);
-    aCos3.setTable(COS2048_DATA);
-    aCos4.setTable(COS2048_DATA);
-    aCos5.setTable(COS2048_DATA);
-    aCos6.setTable(COS2048_DATA);
-    aCos7.setTable(COS2048_DATA);
-    aCos8.setTable(COS2048_DATA);  
-
-    kVol1.setTable(COS2048_DATA);
-    kVol2.setTable(COS2048_DATA);
-    kVol3.setTable(COS2048_DATA);
-    kVol4.setTable(COS2048_DATA);
-    kVol5.setTable(COS2048_DATA);
-    kVol6.setTable(COS2048_DATA);
-    kVol7.setTable(COS2048_DATA);
-    kVol8.setTable(COS2048_DATA);  
-  }
-  else if (sensorVal1 == LOW && sensorVal2 == HIGH) 
-  {
-    aCos1.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos2.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos3.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos4.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos5.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos6.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos7.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    aCos8.setTable(TRIANGLE_VALVE_2_2048_DATA);  
-
-    kVol1.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol2.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol3.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol4.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol5.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol6.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol7.setTable(TRIANGLE_VALVE_2_2048_DATA);
-    kVol8.setTable(TRIANGLE_VALVE_2_2048_DATA);  
-  }
-   else
-  {
-    aCos1.setTable(SAW2048_DATA);
-    aCos2.setTable(SAW2048_DATA);
-    aCos3.setTable(SAW2048_DATA);
-    aCos4.setTable(SAW2048_DATA);
-    aCos5.setTable(SAW2048_DATA);
-    aCos6.setTable(SAW2048_DATA);
-    aCos7.setTable(SAW2048_DATA);
-    aCos8.setTable(SAW2048_DATA);  
-
-    kVol1.setTable(SAW2048_DATA);
-    kVol2.setTable(SAW2048_DATA);
-    kVol3.setTable(SAW2048_DATA);
-    kVol4.setTable(SAW2048_DATA);
-    kVol5.setTable(SAW2048_DATA);
-    kVol6.setTable(SAW2048_DATA);
-    kVol7.setTable(SAW2048_DATA);
-    kVol8.setTable(SAW2048_DATA);  
-  } 
-  
-  aCos1.setFreq((mozziAnalogRead(A0))*8);
-  aCos2.setFreq((mozziAnalogRead(A1))*8);
-  aCos3.setFreq((mozziAnalogRead(A2))*8);
-  aCos4.setFreq((mozziAnalogRead(A3))*8);
-  aCos5.setFreq((mozziAnalogRead(A4))*8);
-  aCos6.setFreq((mozziAnalogRead(A5))*8);
-  aCos7.setFreq((mozziAnalogRead(A6))*8);
-  aCos8.setFreq((mozziAnalogRead(A7))*8);
-  
-   v1 = kVol1.next(); // 
+   v1 = kVol1.next()>>1; // 
    v2 = kVol2.next();
    v3 = kVol3.next();
    v4 = kVol4.next();
